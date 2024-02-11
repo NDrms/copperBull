91f9c12707075dc2b2df236f4cfa28ed5af3bc84
NDrms <namedimas831@gmail.com>
2024-02-11 16:30:06 +0300

    main

--- src/client/java/ndrms/copper_bull/MainClient.java
+++ src/client/java/ndrms/copper_bull/MainClient.java
@@ -2,7 +2,7 @@ package ndrms.copper_bull;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class MainClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
